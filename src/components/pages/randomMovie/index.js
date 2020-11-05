27e7d188fbe437c2d66c080981f308bc53b019cf
Илья <is@inpool.ru>
2020-11-05 05:51:05 +0300

    Structure of pages

---
+++ src/components/pages/randomMovie/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieDetails from '../../movieDetails';
+
+const RandomMovie = () => {
+    return (
+        <div>
+            <MovieDetails />
+        </div>
+    )
+}
+
+export default RandomMovie
